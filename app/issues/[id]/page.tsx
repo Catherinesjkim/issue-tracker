636bdb93dbfd77a8e77d86dbd98ecb22f0415820
Catherine Kim <Catherinesjkim@users.noreply.github.com>
2023-12-20 17:51:14 -0800

    Add markdown preview

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,13 +1,13 @@
-import { PrismaClient } from '@prisma/client';
-import { notFound } from 'next/navigation';
-import React from 'react'
-import { Card, Flex, Heading, Text } from '@radix-ui/themes';
 import IssueStatusBadge from '@/app/components/IssueStatusBadge';
+import { PrismaClient } from '@prisma/client';
+import { Card, Flex, Heading, Text } from '@radix-ui/themes';
+import { notFound } from 'next/navigation';
+import ReactMarkdown from 'react-markdown';
 
 const prisma = new PrismaClient();
 
 interface Props {
-  params: { id: string }
+  params: { id: string };
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
@@ -25,11 +25,11 @@ const IssueDetailPage = async ({ params }: Props) => {
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
-      <Card>
-        <p>{issue.description}</p>
+      <Card className='prose' mt='4'>
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
       </Card>
     </div>
   )
 }
 
-export default IssueDetailPage
+export default IssueDetailPage;
